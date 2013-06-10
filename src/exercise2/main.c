8ac9594c6c88f09e93b2cc6f87a13000c46beb32
Skrogh <s123369@student.dtu.dk>
2013-06-10 14:08:01 +0200

    hov...

--- src/exercise2/main.c
+++ src/exercise2/main.c
@@ -2,6 +2,8 @@
 #include <sio.h>             // special encore serial i/o routines
 
 #define ESC 0x1B
+#define true 1
+#define false 0
 
 void fgcolor(int foreground) {
 /*  Value      foreground     Value     foreground
@@ -57,11 +59,11 @@ void color(int foreground, int background) {
 
 void resetbgcolor() {
 // gray on black text, no underline, no blink, no reverse
-  printf("%c[m", ESC);  
+  printf( "%c[m", ESC );  
 }
 
 void clrscr() {
-	printf( "%c[2J", ESC);
+	printf( "%c[2J", ESC );
 }
 
 void clreol() {
@@ -73,29 +75,54 @@ void gotoxy( int x, int y ) {
 }
 
 void underline( char on ) {
-	if ( on ) 
+	if ( on )
 		printf( "%c[%dm", ESC, 4 ); // underline esc code 4
 	else
-		printf( "%c[%dm", ESC, 0 ); // normal display escape code 0
+		printf( "%c[%dm", ESC, 24 ); // underline esc code 24
 }
 
 void blink( char on ) {
 	if ( on )
 		printf( "%c[%dm", ESC, 5 ); // blink on escape code 5
 	else
-		printf( "%c[%dm", ESC, 0 ); // escape code 0 for normal display
+		printf( "%c[%dm", ESC, 25 ); // blink on escape code 25
 }
 
 void reverse( char on ) {
 	if ( on ) 
-		printf( "%c[%dm", ESC, 7 ); // escape code 7 for reverse video
+	printf( "%c[%dm", ESC, 7 ); // escape code 7 for reverse video
 	else
-		printf( "%c[dm", ESC, 0 ); // escape code 0 for normal display
+	printf( "%c[%dm", ESC, 27 ); // escape code 7 for reverse video
+}
+
+void reset() {
+	printf( "%c[%dm", ESC, 0 ); // underline esc code 0
+}
+
+void textType( char type ) { 
+	printf( "%c[%dm", ESC, type ); // any escapecode
 }
 
 
 void main() {
-	init_uart( _UART0, _DEFFREQ, _DEFBAUD);
-	printf( "Hello word\n");
-	do {} while ( 1 != 2 );
+	init_uart( _UART0, _DEFFREQ, _DEFBAUD );
+	reset();
+	clrscr();
+	printf( "Hello word1\n" );
+	color( 2, 3 );
+	printf( "Hello word2\n" );
+	reset();
+	reverse( true );
+	printf( "Hello word3\n" );
+	reverse( false );
+	printf( "Hello word4\n" );
+	blink( true );
+	printf( "Hello word5\n" );
+	blink( false );
+	underline( true );
+	printf( "Hello word5\n" );
+	underline( false );
+	printf( "Hello word6\n" );
+	
+	while( 1 );
 }

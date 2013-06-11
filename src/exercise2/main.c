d0c7b96e9664799feda1f36f69d74cb617360ba6
overlord <TheExplosiveSheep@gmail.com>
2013-06-11 17:56:43 +0200

    exercise 4 complete

--- src/exercise2/main.c
+++ src/exercise2/main.c
@@ -2,28 +2,32 @@
 #include <sio.h>             // special encore serial i/o routines
 #include "ansi.h"
 
+#define TERMINAL_WIDTH 80
+#define TERMINAL_HEIGHT 25
+#define FP_CONST 3
+
 void main() {
-	long x, y, i, dx, dy;
-	dx = 2;
-	dy = 1;
-	x = 1;
-	y = 1;
+	int x, y, i, dx, dy;
+	dx = 1;
+	dy = 3;
+	x = 1<<FP_CONST;
+	y = 1<<FP_CONST;
 	init_uart( _UART0, _DEFFREQ, _DEFBAUD );
 	reset();
 	clrscr();
 	while( 1 ){
 		x += dx;
 		y += dy;
-		if ( ( x < 1 ) || ( x > 300 ) ) {
+		if ( ( x < 1<<FP_CONST ) || ( x > (TERMINAL_WIDTH - 10)<<FP_CONST  ) ) {
 			dx = -dx;
 		}
-		if ( ( y < 1 ) || ( y > 200 ) ) {
+		if ( ( y < 1<<FP_CONST ) || ( y > (TERMINAL_HEIGHT - 5)<<FP_CONST  ) ) {
 			dy = -dy;
 		}
 
 		clrscr();
-		window( (char)(x>>3), (char)(y>>3), 10, 5, "lol", 1 );
-		for( i = 0; i < 0x1FFF; i++ ){
+		window( (char)(x>>FP_CONST), (char)(y>>FP_CONST), 10, 5, "lol", 1 );
+		for( i = 0; i < 0x5FFF; i++ ){
 			asm("NOP");
 		}
 	}
--- src/exercise4/main.c
+++ src/exercise4/main.c
@@ -1,19 +1,123 @@
 #include <eZ8.h>             // special encore constants, macros and flash routines
 #include <sio.h>             // special encore serial i/o routines
 #include "ansi.h"
+#include "lut.h"
+
+
+#define FIX2_14 int
+#define FIX2_14_SHIFT 14
+#define FIX2_14_MULT( a, b )	( ( (long)(a) * (long)(b) ) >> FIX2_14_SHIFT )
+#define FIX2_14_DIV( a, b )		( ( (long)(a) << FIX2_14_SHIFT ) / (b)
+#define FIX16_16 long
+#define FIX16_16_SHIFT 16
+#define FIX16_16_MULT( a, b )	( ( (long long)(a) * (long long)(b) ) >> FIX16_16_SHIFT )
+#define FIX16_16_DIV( a, b )		( ( (long long)(a) << FIX2_14_SHIFT ) / (b) )
+#define FIX18_14_MULT( a, b )   ( ( (long long)(a) * (long long)(b) ) >> FIX2_14_SHIFT )
+#define cos( angle ) sin( (int)(angle) + 128 )  //cos( x ) = sin( x + 128 ) when 2pi = 512 degrees
+#define VECTOR2( v, a, b ) initVector( (v), (int)(a), (int)(b) )
+
+typedef struct {
+	long x;
+	long y;
+} vector; 
+
+void printFix( long i ) {
+	if ( ( i & 0x80000000) != 0 ) {
+    	printf( "-" );
+		i = ~i + 1;
+	}
+	printf("%ld.%04ld", i>>16, 10000*(unsigned long) (i & 0xffff) >> 16);
+}
+
+long expand( long i ) {
+	return i << 2;
+}
+
+long sin( int angle ) { //angle in 512degrees
+	int modangle = angle & 0x1ff; // loop over so 0<=modangle<512
+	return SIN[modangle];
+}
+
+void rotate( vector * vectorptr, int deg ) {
+	long cosdeg = cos( deg ); //convert 2.14 from sin() to 18.14
+	long sindeg = sin( deg ); // convert 2.14 from sin() to 18.14
+	long tempx = vectorptr -> x;
+	long tempy = vectorptr -> y;
+	vectorptr -> x = (long) ( (long) FIX18_14_MULT( tempx, cosdeg ) - (long) FIX18_14_MULT( tempy, sindeg ) );
+	vectorptr -> y = (long) ( (long ) FIX18_14_MULT( tempx, sindeg ) + (long) FIX18_14_MULT( tempy, cosdeg ) );
+}
+
+void initVector( vector* vec, int x, int y ) {
+	vec -> x = (long) x << 14;	   //convert to 18_14
+	vec -> y = (long) y << 14;
+}
+	
 
 void main() {
-	char a, b, c, d, e;
-	int f;
+	vector * vector1;
+	vector * vector2;
+	vector * vector3;
+	vector * vector4;
 	init_uart( _UART0, _DEFFREQ, _DEFBAUD );
-	reset();
 	clrscr();
-	a = 64;
-	b = 64;
-	c = (a * b) >> 8;
-	d = ((int)(a) * (int)(b)) >> 8;
-	e = (int)(a * b) >> 8;
-	f = (a * b) >> 8;
-	printf("%d | %d | %d | %d", c, d, e, f );
+	
+	VECTOR2( vector1, 1, 2 );
+	VECTOR2( vector2, 6, 4 );
+	VECTOR2( vector3, -4, -4 );
+	VECTOR2( vector4, -4, 2 );
+	printf( "Hej mor, se mig, jeg er i terminalen <3 \n" );
+/*
+	printFix( expand( sin( 0 ) ) ); 
+	printf( "\n" );
+	printFix( expand( sin( 64 ) ) ); 
+	printf( "\n" );
+	printFix( expand( sin( -111 ) ) ); 
+	printf( "\n" );
+	printFix( expand( sin( 923 ) ) ); 
+	printf( "\n" );
+
+	printFix( expand( cos( 0 ) ) ); 
+	printf( "\n" );
+	printFix( expand( cos( 64 ) ) ); 
+	printf( "\n" );
+	printFix( expand( cos( -111 ) ) ); 
+	printf( "\n" );
+	printFix( expand( cos( 923 ) ) ); 
+	printf( "\n" );
+	printFix( expand( sin( 256 ) ) ); 
+	printf( "\n" );
+	printFix( expand( cos( 256 ) ) ); 
+	printf( "\n" );
+
+*/	
+	rotate( vector1, 256 );
+	printf( "vector1 \n" );
+	printFix( vector1 -> x << 2 );
+	printf( " , " );
+	printFix( vector1 -> y << 2 );
+	printf( "\n" );
+
+	rotate( vector2, -14 );
+	printf( "vector2 \n" );
+	printFix( vector2 -> x << 2 );
+	printf( " , " );
+	printFix( vector2 -> y << 2 );
+	printf( "\n" );
+
+	rotate( vector3, 1280 );
+	printf( "vector3 \n" );
+	printFix( vector3 -> x << 2 );
+	printf( " , " );
+	printFix( vector3 -> y << 2 );
+	printf( "\n" );
+	
+	rotate( vector4, -50 );
+	printf( "vector4 \n" );
+	printFix( vector4 -> x << 2 );
+	printf( " , " );
+	printFix( vector4 -> y << 2 );
+	printf( "\n" );
+	
+	printf( "Victory!!! \n" );
 	while(1);
 }
